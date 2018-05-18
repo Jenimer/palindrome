54650a6cecb80c5842d586880d7b410d8b95b82f
Jen Broadhead <jeniffer.broadhead@gmail.com>
2018-05-18 10:28:18 -0600

    palindrome

---
+++ palindrome.rb
@@ -0,0 +1,23 @@
+def user_input
+    input #.lowercase method (so case sensitivity isn't an issue)
+end
+
+def array
+   arr = [] #input gets shoved into the array
+            #with the string split into individual characters
+        
+end
+
+def shove
+    arr << input #use proper syntax
+end
+
+puts "enter word:" #input from user
+input = gets.chomp
+
+#get the first and last array index and compare
+arr.first
+arr.last
+
+if arr.first = arr.last
+    
